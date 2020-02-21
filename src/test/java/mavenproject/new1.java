5bd07a0ce96d009dd56a9a38621c365b076b06bf
marcellin93 <marcellinroi1993@gmail.com>
2020-02-21 19:20:51 +0530

    mom

--- src/test/java/mavenproject/new1.java
+++ src/test/java/mavenproject/new1.java
@@ -14,5 +14,6 @@ public class new1 extends baseclass{
 		click(login);
 		System.out.println("marcellin");
 		System.out.println("marie");
+		System.out.println("mom**************************");
 }
 }
