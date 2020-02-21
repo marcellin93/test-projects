77a677e4fb7f9d50049a1b41559f88bdf626e534
marcellin93 <marcellinroi1993@gmail.com>
2020-02-21 16:49:13 +0530

    add name

--- src/test/java/mavenproject/new1.java
+++ src/test/java/mavenproject/new1.java
@@ -17,7 +17,7 @@ public class new1 extends baseclass{
 	    System.out.println("****************** 3rd update **********************");
 	    System.out.println("******************** new 3rd update **************************");
 	    System.out.println("******************///////////////////////////////////*********************");
-	    
+	    System.out.println("marcellin");
    
 }
 }
